fab134ddec2897f06960e2fbceb3b157294e456e
racwade <racwade@yahoo.com>
2016-11-24 16:45:53 -0500

    Scraper functionality now loads files to mongodb. Added a couple of utility functions for converting from times to binary (more coming...)

--- main.js
+++ main.js
@@ -7,6 +7,47 @@ var cheerio = require('cheerio');
 var app     = express();
 var numclasses = 0;
 
+function binarifyTime(time){
+    var shifts = -14; //Doors open at 7:00. Therefore, a time of 7:00 will represent 0 shifts.
+    if (time.indexOf("pm") != -1) {
+        shifts += 24; //If in the evening, shift 12 more hours (24 places).
+    }
+    if (time.indexOf("30") != -1 || time.indexOf("40") != -1) {
+        shifts += 1; //If on the half-hour, shift by 1.
+    }
+    var hours = parseInt(time.split(":")[0]);
+    if (hours != 12) shifts += hours * 2; // skip 12:00, for obvious reasons
+    var number = 1 << shifts;
+    return number;
+}
+function binarifyDuration(starttime,endtime){
+    var firstshifts = -14; //As per binarifyTime, but for the first number only - and without actually doing the shifting.
+    if (starttime.indexOf("pm") != -1) {
+        firstshifts += 24;
+    }
+    if (starttime.indexOf("30") != -1 || starttime.indexOf("40") != -1) {
+        firstshifts += 1;
+    }
+    var hours = parseInt(starttime.split(":")[0]);
+    if (hours != 12) firstshifts += hours * 2;
+    
+    var lastshifts = -14; //Now we do this again, for the end time.
+    if (endtime.indexOf("pm") != -1) {
+        lastshifts += 24;
+    }
+    if (endtime.indexOf("30") != -1 || endtime.indexOf("40") != -1) {
+        lastshifts += 1;
+    }
+    
+    hours = parseInt(endtime.split(":")[0]);
+    if (hours != 12) lastshifts += hours * 2;
+    var number = 0;
+    for (var i = firstshifts; i <= lastshifts; i++){
+        number += (1 << i); //sum all the numbers between the start and end time, inclusive
+    }
+    return number;
+}
+
 app.get('/sample', function(req, res){
     // Placeholder URL for now.
 
@@ -27,72 +68,96 @@ app.get('/sample', function(req, res){
             var lasttime = "ERROR";
             var lastday = "ERROR";
             var lastroom = "ERROR";
-            var finaldiv = "<ul>";
             
             //We need to work with "MongoClient" interface in order to connect to a mongodb server.
             var MongoClient = mongodb.MongoClient;
             // Connection URL. This is where your mongodb server is running.
-            var db_url = 'mongodb://localhost:27017/my_database_name';
-            MongoClient.connect(db_url, function (err, db) {
-                if (err) {
-                    console.log('Unable to connect to the mongoDB server. Error:', err);
-                } else {
-                    //Connection succeeded.
-                    console.log('Connection established to', db_url,"\n");
-
-                    // do some work here with the database.
-
-                    
-                    $("table.bordertable").find("tr").each(function(){
-                        var cols = $(this).find("td.dbdefault");
-                        if (cols.length == 18){
-
-                            // Only get the class listings - that is, items with 18 column entries. (A bit of a cheat, but hey.)
-                            if($(cols[2]).text() != "\xa0" && $(cols[3]).text() != "\xa0"){
-                                // If there's new class info, update it.
-                                lastclass = $(cols[2]).text() + " " + $(cols[3]).text();
-                            }
-                            if($(cols[9]).text() != "\xa0"){
-                                // If there's new day info, update it.
-                                lastday = $(cols[9]).text();
-                            }
-                            if($(cols[10]).text() != "\xa0"){
-                                // If there's new time info, update it.
-                                lasttime = $(cols[10]).text();
-                            }
-                            if($(cols[16]).text() != "\xa0"){
-                                // If there's new room info, update it... but this one's a bit more complex.
-                                if($(cols[16]).text() != "TBA"){
-                                    if($($(cols[16]).children("br").get(1).nextSibling).text() != ""){
-                                        //If there's more than one BR element, get what's after the second one.
-                                        lastroom = $($(cols[16]).children("br").get(1).nextSibling).text();
-                                    } else {
-                                        //If there's one BR element, get what's after the first one.
-                                        lastroom = $($(cols[16]).children("br").get(0).nextSibling).text();
-                                    }
-                                } else {
-                                    //If the room is TBD, just copy the full thing.
-                                    lastroom = $(cols[16]).text();
-                                }
-                            }
-                            //finaldiv += "<li>" + lastclass + ": " + lastday + " " + lasttime + " (" + lastroom + ")" + "</li>";
-                            //console.log(finaldiv += "<li>" + lastclass + ": " + lastday + " " + lasttime + " (" + lastroom + ")" + "</li>");
-                            console.log(lastclass + "\n");
-                            numclasses++; //Increment number of classes.
+            
+            var Server = mongodb.Server, database = mongodb.Db, BSON = mongodb.BSONPure;
+            var server = new Server('localhost', 27017, {auto_reconnect: true});
+            db = new database('coursedb', server);
+            
+            db.open(function(err,db){
+                if (!err) {
+                    console.log("Connected to 'coursedb' database");
+                    db.collection('courses', {strict: true}, function(err, collection) {
+                        if (err) {
+                            console.log("The 'courses' collection doesn't exist. Creating it with sample data...");
                         }
+                        else {
+                            db.collection('courses').remove({});
+                            db.collection('rooms').remove({});
+                        }
+                        
+                        //parse the collection
+                        if($("table.bordertable").find("tr").length == 0){
+                            console.log("ERROR: Length of the loaded document found to be 0. Something weird's going on!")
+                        }
+                        var i = 0;
+                        $("table.bordertable").find("tr").each(function(){
+                            var cols = $(this).find("td.dbdefault");
+                            if (cols.length == 18){
+
+                                // Only get the class listings - that is, items with 18 column entries. (A bit of a cheat, but hey.)
+                                if($(cols[2]).text() != "\xa0" && $(cols[3]).text() != "\xa0"){
+                                    // If there's new class info, update it.
+                                    lastclass = $(cols[2]).text() + " " + $(cols[3]).text();
+                                }
+                                if($(cols[9]).text() != "\xa0"){
+                                    // If there's new day info, update it.
+                                    lastday = $(cols[9]).text();
+                                }
+                                if($(cols[10]).text() != "\xa0"){
+                                    // If there's new time info, update it.
+                                    lasttime = $(cols[10]).text();
+                                }
+                                if($(cols[16]).text() != "\xa0"){
+                                    // If there's new room info, update it... but this one's a bit more complex.
+                                    if($(cols[16]).text() != "TBA"){
+                                        if($($(cols[16]).children("br").get(1).nextSibling).text() != ""){
+                                            //If there's more than one BR element, get what's after the second one.
+                                            lastroom = $($(cols[16]).children("br").get(1).nextSibling).text();
+                                        } else {
+                                            //If there's one BR element, get what's after the first one.
+                                            lastroom = $($(cols[16]).children("br").get(0).nextSibling).text();
+                                        }
+                                    } else {
+                                        //If the room is TBD, just copy the full thing.
+                                        lastroom = $(cols[16]).text();
+                                    }
+                                }
+                                var bindur = 0;
+                                if(lasttime != "TBA"){
+                                    var times = lasttime.split("-");
+                                    bindur = binarifyDuration(times[0],times[1]);
+                                    //console.log(times[0] + " " + times[1] + ": " + bindur);
+                                    db.collection("courses").save( {
+                                        "class" : lastclass,
+                                        "day" : lastday,
+                                        "times" : lasttime,
+                                        "binarytimes" : bindur,
+                                        "room" : lastroom
+                                    });
+                                    numclasses++; //Increment number of classes.
+                                    db.collection("rooms").update({"room" : lastroom}, 
+                                        {$bit : {"times" : { or : bindur}}},
+                                        {"new":true, "upsert":true}
+                                    );
+                                };
+                            }
+                        });
+                        //Close connection
+                        db.close();
+                        console.log("Connection closed.");
                     });
-                    //Close connection
-                    db.close();
-                    console.log("Connection closed.");
                 }
             });
             console.log(numclasses);
-            finaldiv += "</ul>";
             res.send('Check your console! And your database!');
         }
     })
 });
 
 app.listen('8081')
-console.log('Magic happens on port 8081');
+console.log('Connected on port 8081.');
 exports = module.exports = app;
